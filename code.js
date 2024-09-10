1bc5068eb7c3a95985314b642641f76810fc0dbd
Nandan Vallamdasu <101306480+nandan2003@users.noreply.github.com>
2024-09-10 21:35:45 +0530

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ var count = 0;
 var btnSize = 18;
 
 function noFunc() {
-  wordArray = ["Serious ga na?", "Po please", "Naaku evaru unnaru cheppu 🥺", "Ego na?", "Ardham chesukovachu ga please", "Nen alcohol taganu", "No cigarettes", "Naku kuda Krishnudu ante chala istam", "Naky Raadha ante kuda istam", "Ni lani ammayi nenu na Wife laga korukunna"];
+  wordArray = ["Serious ga na?", "Po please", "Naaku evaru unnaru cheppu 🥺", "Ego na?", "Ardham chesukovachu ga please", "Nen alcohol taganu", "No cigarettes", "Naku kuda Krishnudu ante chala istam", "Naky Raadha ante kuda istam", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna", "Ni lanti ammayi ni nenu na Wife laga korukunna"];
   document.getElementById("btn2").innerHTML = `${wordArray[count]}`;
   count++;
   document.getElementById("btn1").style.fontSize = `${(btnSize += 20)}px`;
